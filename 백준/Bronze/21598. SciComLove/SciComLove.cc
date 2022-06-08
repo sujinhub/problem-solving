a9cbcd23ed0bfce2677cdb74834c0006b7347e0f
ssujin-park <82197800+ssujin-park@users.noreply.github.com>
2022-06-08 20:12:23 +0900

    [Bronze IV] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N; cin >> N;
+	while (N--) {
+		cout << "SciComLove\n";
+	}
+
+	return 0;
+}
