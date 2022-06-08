b9ffeb346fc01de99a4255e2d1f700fa8da8cd17
ssujin-park <82197800+ssujin-park@users.noreply.github.com>
2022-06-08 20:02:01 +0900

    [Bronze III] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int t; cin >> t;
+	while (t--) {
+		int x, y; cin >> x >> y;
+		cout << x + y << '\n';
+	}
+
+	return 0;
+}
