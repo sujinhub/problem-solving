d205254faaca644eb834d97af94c90b87093c4c7
ssujin-park <82197800+ssujin-park@users.noreply.github.com>
2022-06-08 20:05:53 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int X, N; cin >> N;
+	queue<int> queue;
+	string cmd;
+
+	for (; N--;) {
+		cin >> cmd;
+		if (cmd == "push") {
+			cin >> X;
+			queue.push(X);
+		}
+		else if (cmd == "pop") {
+			if (queue.empty()) cout << "-1\n";
+			else {
+				cout << queue.front() << '\n';
+				queue.pop();
+			}
+		}
+		else if (cmd == "size") {
+			cout << queue.size() << '\n';
+		}
+		else if (cmd == "empty") {
+			if (queue.empty()) cout << "1\n";
+			else cout << "0\n";
+		}
+		else if (cmd == "front") {
+			if (queue.empty()) cout << "-1\n";
+			else cout << queue.front() << '\n';
+		}
+		else if (cmd == "back") {
+			if (queue.empty()) cout << "-1\n";
+			else cout << queue.back() << '\n';
+		}
+	}
+	return 0;
+}
