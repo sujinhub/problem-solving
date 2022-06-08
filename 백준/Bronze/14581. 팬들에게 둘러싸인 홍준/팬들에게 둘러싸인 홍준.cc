5e6afc71a05f1266f9afcef3d36a27cda638394e
ssujin-park <82197800+ssujin-park@users.noreply.github.com>
2022-06-08 20:13:58 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string id; cin >> id;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << id << "::fan:\n";
+	cout << ":fan::fan::fan:";
+
+	return 0;
+}
