ad7fec8114bf9cf3f2f53c7e1c5b629fc2c2bdb6
ssujin-park <82197800+ssujin-park@users.noreply.github.com>
2022-06-08 20:14:29 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int N, M; // 4 2
+int *SEQ;
+int SEQ_idx;
+
+// location에 넣어도 되는지 from부터 확인
+void DECIDE(int from, int location) { 
+	for (int i = from; i <= N; i++) {
+		for (int j = location; j < M; j++) SEQ[j] = 0;
+		SEQ[location] = i;
+		if (location + 1 == M) {
+			for (int i = 0; i < M; i++) printf("%d ", SEQ[i]);
+			printf("\n");
+		}
+		else DECIDE(i + 1, location + 1);
+	}
+
+}
+
+int main(void) {
+	scanf("%d %d", &N, &M);
+	
+	SEQ = (int *)malloc(sizeof(int)*M);
+	memset(SEQ, 0, sizeof(int)*M);
+
+	DECIDE(1, 0); // 1부터 N까지 SEQ[0]에 넣을 값 확인
+}
