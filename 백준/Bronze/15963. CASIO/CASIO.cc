da2f92a6acee59287d900475bea2c16e73d4cc93
ssujin-park <82197800+ssujin-park@users.noreply.github.com>
2022-06-08 20:15:33 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	long long n, m; cin >> n >> m;
+	if (n == m) cout << 1;
+	else cout << 0;
+
+	return 0;
+}
