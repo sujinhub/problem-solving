95d92bdcb3e84734709ab47afd85dc9047d3071c
ssujin-park <82197800+ssujin-park@users.noreply.github.com>
2022-06-08 19:52:58 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string str; cin >> str;
+	cout << str.length();
+
+	return 0;
+}
