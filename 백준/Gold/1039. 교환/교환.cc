f4bfd415a1f53e295b07647be2b41fc14f8d3fe6
박수진 <82197800+sujinhub@users.noreply.github.com>
2022-07-05 06:25:00 +0900

    Update 교환.cc

--- 백준/Gold/1039. 교환/교환.cc
+++ 백준/Gold/1039. 교환/교환.cc
@@ -1,29 +1,24 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
 #include <cstring>
-
+#include <algorithm>
 using namespace std;
 
 string N;
-int K;
-int dp[1000001][11];
+int K, dp[1000001][11];
 
-int dfs(string a, int depth) {
-	if (depth == K) {
-		return stoi(a);
-	}
-	int& ret = dp[stoi(a)][depth];
+int DFS(string str, int depth) {
+	if (depth == K) return stoi(str);
+
+	int& ret = dp[stoi(str)][depth];
 	if (ret != -1) return ret;
 
-	for (int i = 0; i < a.size(); i++)
-	{
-		for (int j = i + 1; j < a.size(); j++)
-		{
-			if (a[j] == '0' && i == 0)    continue;
-			swap(a[i], a[j]);
-			ret = max(ret, dfs(a, depth + 1));
-			swap(a[i], a[j]);
+	for (int i = 0; i < str.size(); i++) {
+		for (int j = i + 1; j < str.size(); j++) {
+			if (str[j] == '0' && !i) continue;
+			swap(str[i], str[j]);
+			ret = max(ret, DFS(str, depth + 1));
+			swap(str[i], str[j]);
 		}
 	}
 	return ret;
@@ -35,7 +30,7 @@ int main() {
 
 	cin >> N >> K;
 	memset(dp, -1, sizeof(dp));
-	cout << dfs(N, 0);
+	cout << DFS(N, 0);
 
 	return 0;
 }
