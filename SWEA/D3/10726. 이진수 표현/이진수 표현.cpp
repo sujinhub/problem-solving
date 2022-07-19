dae568f1e98e4d8e538244b1434eb27dc6672709
박수진 <82197800+sujinhub@users.noreply.github.com>
2022-07-19 17:37:56 +0900

    [D3] Title: 이진수 표현, Time: 10 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -9,12 +9,8 @@ int main() {
 	for (int test_case = 1; test_case <= TC; test_case++) {
 		int N, M; cin >> N >> M;
 		string ans = "ON";
-		for (int i = 0; i < N; i++) {
-			if (!(M & (1 << i))) {
-				ans = "OFF";
-				break;
-			}
-		}
+		int bit = (1 << N) - 1;
+		if (((M & bit) != bit)) ans = "OFF";
 		cout << '#' << test_case << ' ' << ans << '\n';
 	}
 
