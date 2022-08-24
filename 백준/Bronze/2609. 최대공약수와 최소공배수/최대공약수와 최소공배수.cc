c835266d21abb31e8fbf4fcc43377e7a7a640ded
박수진 <82197800+sujinhub@users.noreply.github.com>
2022-08-24 20:58:42 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,27 +1,18 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	int N, M; cin >> N >> M;
-	int max, min;
-	if (N > M) {
-		max = N;
-		min = M;
-	}
-	else {
-		max = M;
-		min = N;
-	}
-
+	int mx = max(N, M);
+    int mn = min(N, M);
+    
 	int s = 0;
-	for (int i = 1; i <= min; i++) {
-		if (min % i == 0 && max % i == 0) {
-			s = i;
-		}
-	}
+	for (int i = 1; i <= mn; i++)
+		if (!(mn % i) && !(mx % i)) s = i;
 
 	int m = M * N / s;
-
 	cout << s << '\n' << m;
-
+    
+    return 0;
 }
