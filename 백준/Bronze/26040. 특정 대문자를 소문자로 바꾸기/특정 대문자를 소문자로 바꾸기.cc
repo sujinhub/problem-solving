8407b37aef4f4eb0e68f3e4651a8be2c80a4560f
박수진 <82197800+sujinhub@users.noreply.github.com>
2022-11-22 05:55:35 +0900

    [Bronze III] Title: 특정 대문자를 소문자로 바꾸기, Time: 4 ms, Memory: 2352 KB -BaekjoonHub

---
+++ 백준/Bronze/26040. 특정 대문자를 소문자로 바꾸기/특정 대문자를 소문자로 바꾸기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool flag[26];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string A, B, buf;
+	getline(cin, A);
+	getline(cin, B);
+	istringstream ss(B);
+
+	while (getline(ss, buf, ' ')) flag[buf[0] - 'A'] = true;
+	for (int i = 0; i < A.length(); i++) {
+		if (flag[A[i] - 'A'] && A[i] >= 'A' && A[i] <= 'Z') cout << (char)(A[i] + 'a' - 'A');
+		else cout << (char)A[i];
+	}
+
+	return 0;
+}
